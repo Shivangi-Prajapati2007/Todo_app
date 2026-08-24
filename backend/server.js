48d27350af9b4bcf76ddeebdaf4911d76bf8975a
Shivangi Prajapati <shivangiprajapati395@gmail.com>
2026-08-24 20:28:15 +0530

    server error resolve

--- backend/server.js
+++ backend/server.js
@@ -13,22 +13,20 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req , res) => res.send('Server is running'))
+app.get("/", (req, res) => {
+  res.send("Server is running");
+});
 
 app.use("/api/auth", authRoutes);
 app.use("/api/tasks", taskRoutes);
-
-const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(PORT, () => {
-      console.log(`Server running on http://localhost:${PORT}`);
-    });
   })
   .catch((error) => {
     console.error("MongoDB connection failed:", error.message);
-    process.exit(1);
   });
+
+export default app;
